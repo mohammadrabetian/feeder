19d31b39ee9b337f4b3768336be1e5efc3707a05
mohammadrabetian <tommyrabetian@gmail.com>
2020-12-01 21:37:01 +0330

    Test helpers

---
+++ feeds/tests/test_helpers.py
@@ -0,0 +1,28 @@
+import pytest
+from django.core.exceptions import ValidationError
+from faker import Faker
+from feedparser import FeedParserDict
+from feeds.exceptions import NotValidFeedUrl
+from feeds.helpers import request_feed, validate_url
+from requests.exceptions import ConnectionError, ReadTimeout
+
+fake = Faker()
+
+
+def test_request_feed_exceptions():
+    fake_url = fake.url()
+    with pytest.raises((ConnectionError, ReadTimeout, NotValidFeedUrl)):
+        request_feed(feed_url=fake_url)
+
+
+def test_request_feed_return_value():
+    feed_url = "http://www.nu.nl/rss/Algemeen"
+    response = request_feed(feed_url=feed_url)
+    assert isinstance(response, FeedParserDict)
+    assert not response.bozo
+
+
+def test_validate_url():
+    invalid_url = "ObviouslyFake"
+    with pytest.raises(ValidationError):
+        validate_url(invalid_url)
